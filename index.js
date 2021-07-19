b2d07a3c32a7ec1d882c12f699ecb32087855adb
faizalanwar <fzlanwr@gmail.com>
2021-07-19 12:08:07 +0700

    update and styling

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
 
     const bookContainer = document.querySelector('#book-container')
-    const status = document.getElementById('#status')
 
     const submit = document.getElementById('#submit')
     const bookURL = 'http://localhost:3000/tamu'
     // const bookURL = 'https://anww.herokuapp.com/tamu'
-
-   
     const bookForm = document.querySelector('#book-form')
     let allBooks = []
     
@@ -35,9 +32,16 @@ document.addEventListener('DOMContentLoaded', function () {
 
     bookForm.addEventListener('submit', (e) => {
       e.preventDefault()
-    
-    
-      console.log(e.target)
+      
+      document.getElementById("alert").outerHTML=`<div class="alert alert-success" id="alert" role="alert">
+          A simple success alert—check it out!
+      </div>`
+      // console.log(e.target)
+      window.setTimeout(function(){
+        document.getElementById("alert").classList.add("none");
+      }, 5000);
+      
+ 
 
       const titleInput = bookForm.querySelector('#title').value
       const authorInput = bookForm.querySelector('#author').value
@@ -65,9 +69,10 @@ document.addEventListener('DOMContentLoaded', function () {
             <td><img src="${book.coverImage}" width="30px" height="50px"></td>
             <td>${book.description}</td>
             <td>         
-            <button data-id="${book.id}" id="edit-${book.id}" data-action="edit">Edit</button>
-            <button data-id="${book.id}" id="delete-${book.id}" data-action="delete">Delete</button>
+            <button data-id="${book.id}" id="edit-${book.id}" data-action="edit" class="btn btn-success">Edit</button>
+            <button data-id="${book.id}" id="delete-${book.id}" data-action="delete" class="btn btn-danger">Delete</button>
             </td>
+  
           </tr>`
         })
       
@@ -80,18 +85,61 @@ document.addEventListener('DOMContentLoaded', function () {
         const editButton = document.querySelector(`#edit-${e.target.dataset.id}`)
       editButton.disabled = true
       
+   
       const bookData = allBooks.find((book) => {
         return book.id == e.target.dataset.id
       })
       e.target.parentElement.innerHTML += `
       <div id='edit-book'>
-        <form id="book-form">
+        <form id="edit-form">
           <input required id="edit-title" placeholder="${bookData.title}">
           <input required id="edit-author" placeholder="${bookData.author}">
           <input required id="edit-coverImage" placeholder="${bookData.coverImage}">
           <input required id="edit-description" placeholder="${bookData.description}">
           <input type="submit" value="Edit Book">
       </div>`
+
+      const editForm = document.querySelector('#edit-form')
+    
+    editForm.addEventListener("submit", (e) => {
+      e.preventDefault()
+      const titleInput = document.querySelector("#edit-title").value
+      const authorInput = document.querySelector("#edit-author").value
+      const coverImageInput = document.querySelector("#edit-coverImage").value
+      const descInput = document.querySelector("#edit-description").value
+      const editedBook = document.querySelector(`#book-${bookData.id}`)
+      fetch(`${bookURL}/${bookData.id}`, {
+        method: 'PATCH',
+        body: JSON.stringify({
+          title: titleInput,
+          author: authorInput,
+          coverImage: coverImageInput,
+          description: descInput
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then( response => response.json() )
+      .then( book => {
+        editedBook.innerHTML = `
+        <tr id="book-${book.id}">
+        <td>${book.id}</td>
+          <td>${book.title}</td>
+          <td>Author: ${book.author}</td>
+          <td><img src="${book.coverImage}" width="30px" height="50px"></td>
+          <td>${book.description}</td>
+          <td>         
+          <button data-id="${book.id}" id="edit-${book.id}" data-action="edit" class="btn btn-success">Edit</button>
+          <button data-id="${book.id}" id="delete-${book.id}" data-action="delete" class="btn btn-danger">Delete</button>
+          </td>
+
+        </tr>
+        <div id=edit-book-${book.id}>
+        </div>`
+        editForm.innerHTML = ""
+      })
+  }) // end of this event Listener for edit submit
+
       } else if (e.target.dataset.action === 'delete') {
         document.querySelector(`#book-${e.target.dataset.id}`).remove()
         
@@ -103,47 +151,6 @@ document.addEventListener('DOMContentLoaded', function () {
         }).then( response => response.json())
       }
     })
-
-
-
-  //   editForm.addEventListener("submit", (e) => {
-  //     event.preventDefault()
-  //     const titleInput = document.querySelector("#edit-title").value
-  //     const authorInput = document.querySelector("#edit-author").value
-  //     const coverImageInput = document.querySelector("#edit-coverImage").value
-  //     const descInput = document.querySelector("#edit-description").value
-  //     const editedBook = document.querySelector(`#book-${bookData.id}`)
-  //     fetch(`${bookURL}/${bookData.id}`, {
-  //       method: 'PATCH',
-  //       body: JSON.stringify({
-  //         title: titleInput,
-  //         author: authorInput,
-  //         coverImage: coverImageInput,
-  //         description: descInput
-  //       }),
-  //       headers: {
-  //         'Content-Type': 'application/json'
-  //       }
-  //     }).then( response => response.json() )
-  //     .then( book => {
-  //       editedBook.innerHTML = `
-  //       <div id=book-${book.id}>
-  //         <h2>${book.title}</h2>
-  //         <h4>Author: ${book.author}</h4>
-  //         <img src="${book.coverImage}" width="333" height="500">
-  //         <p>${book.description}</p>
-  //         <button data-id=${book.id} id="edit-${book.id}" data-action="edit">Edit</button>
-  //         <button data-id=${book.id} id="delete-${book.id}" data-action="delete">Delete</button>
-  //       </div>
-  //       <div id=edit-book-${book.id}>
-  //       </div>`
-  //       editForm.innerHTML = ""
-  //     })
-  // }) // end of this event Listener for edit submit
-
-
-
-
 
 
 
